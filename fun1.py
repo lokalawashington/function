0a7f993ea26e3cf13fb36e2498b5c4695f9b9893
peter ngotho <sammykamau772@mail.com>
2021-05-26 02:49:22 -0400

    Initial commit

---
+++ fun1.py
@@ -0,0 +1,8 @@
+def format_name(f_name, l_name):
+    formated_f_name = f_name.title()
+    formated_f_name = l_name.title()
+    print(f"{formated_f_name} {formated_f_name}")
+    # .title(transforms every first letter in a word into uppercase)
+
+
+format_name("washingon", "lokala")
